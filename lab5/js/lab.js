491c63b5b19cbea58715308ed565251c6792f966
mlencioni1 <78108780+mlencioni1@users.noreply.github.com>
2021-09-15 11:35:17 -0700

    Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,26 @@
+// This is our Lab 5: playing with variables.
+// Our goal is to define and declare elements, .
+// @link   img/lab4/index.html
+// @file   This files defines the MyClass class.
+// @author MatthewLencioni.
+// @since  09.13.2021
+
+// Define the Variables
+var make = "Honda";
+var model = "Civic";
+var color = "Black";
+var year = 2001;
+var ownIt = true;
+
+// calculate
+var age = 2021 - year;
+20
+console.log(age);
+20
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");

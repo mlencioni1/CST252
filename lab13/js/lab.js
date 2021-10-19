c7a92d22f53fa1bd8c7cf8521678803fa403a63b
mlencioni1 <78108780+mlencioni1@users.noreply.github.com>
2021-10-19 00:57:11 -0700

    lab13.1.3

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -8,6 +8,10 @@
 var total = 0;
 
 // QUESTION 1
+//on button click:
+//  Hide other options
+//  Add value to total
+//  disable button from multiple clicks
 $("#quiet").click(function(){
   $("#leveled").hide();
   $("#loud").hide();
@@ -28,6 +32,10 @@ $("#loud").click(function(){
 });
 
 //QUESTION 2
+//on button click:
+//  Hide other options
+//  Add value to total
+//  disable button from multiple clicks
 $("#snack").click(function(){
   $("#plate").hide();
   $("#seconds").hide();
@@ -48,6 +56,10 @@ $("#seconds").click(function(){
 });
 
 // Question 3
+//on button click:
+//  Hide other options
+//  Add value to total
+//  disable button from multiple clicks
 $("#middle").click(function(){
   $("#firm").hide();
   $("#soft").hide();
@@ -72,12 +84,27 @@ $("#firm").click(function(){
 // final output
 $("#final").click(function(){
   $("#output").empty();
-  // 1-3: Baby Bear, 4-6: Mama Bear, 7-9 Papa Bear
+  //I wanted the chances of choice to be balanced
+  //  3 questions = 3 minimum, 9 maximum total output
+  //    3 has 1 combinations
+  //    4 has 3 combinations
+  //    5 has 3 combinations
+  //    6 has 6 combinations
+  //    7 has 3 combinations
+  //    8 has 3 combinations
+  //    9 has 1 combinations
+
   if (total <= 4) {
   $("#output").append("Baby Bear's Chair Fits You!");
-} else if (total >= 8){
+  // 3-4: Baby Bear (4 combinations) since baby bear is smallest it should get
+  //      the least chance of getting picked.
+} else if (total >= 7){
   $("#output").append("Papa Bear's Chair Fits You!");
+  // 5-6: Mama Bear (9 combinations) since mama bear is more moderate, shes
+  //      most average therefore gets the highest chance of being picked.
   } else {
   $("#output").append("Mama Bear's Chair Fits You!");
+  // 7-9: Papa Bear (7 combinations) since papa bear is on the high end, and
+  //      is more likely to be related to than baby bear his chances are in the middle.
   }
 });

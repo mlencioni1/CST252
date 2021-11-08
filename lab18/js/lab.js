441d2584fc99b6af28de187c6c71a3566c263929
mlencioni1 <78108780+mlencioni1@users.noreply.github.com>
2021-11-08 12:02:35 -0800

    lab 18

---
+++ lab18/js/lab.js
@@ -0,0 +1,6 @@
+// Lab18.js - Libraries & Frameworks
+// Our goal use bootstrap to create a NAV & Grids
+// @link   img/lab18/index.html
+// @file   This files defines the MyClass class.
+// @author MatthewLencioni.
+// @since  11.08.2021
